9fb154c8070b283e837e961cd176018f8c8cba8c
Dominik Berner <dominik.berner@gmail.com>
2020-12-28 22:04:13 +0100

    Adapt build/run script to pull images from dockerhub

--- build_container.sh
+++ build_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # build without context to speed up and because no files are needed in the Dockerfile
-docker build --rm -t yoctobuilder - < Dockerfile
+docker build --rm -t bernedom/yoctobuilder:hack - < Dockerfile
